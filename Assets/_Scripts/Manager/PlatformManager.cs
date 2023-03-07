c817058353f8057bd154cc6b4d0167437177e7cc
ShunPeng1 <shunpeng1@gmail.com>
2023-03-07 21:50:19 +0700

    refactor: Resource Manager and Platform Manager

---
+++ Assets/_Scripts/Manager/PlatformManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformManager : Singleton<PlatformManager>
+{
+
+
+    public void CreateNewPlatform()
+    {
+        
+    }
+    
+}
--- Assets/_Scripts/Manager/ResourceManager.cs
+++ Assets/_Scripts/Manager/ResourceManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlatformManager : MonoBehaviour
+public class ResourceManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
