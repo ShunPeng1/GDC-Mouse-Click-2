04f1e71a0ceafb2cf28c96aab0fb75506cf1bcdc
hathienhai4 <120299076+hathienhai4@users.noreply.github.com>
2023-03-08 19:48:07 +0700

    lan2

--- Assets/_Scripts/Ball.cs
+++ Assets/_Scripts/Ball.cs
@@ -11,16 +11,21 @@ public class Ball : MonoBehaviour
         InitialPos = transform.position;
     }
 
-    private void OnMouseDrag()
-    {
-        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        transform.position = new Vector3(mousePos.x, mousePos.y, 0);
-    }
+    // private void OnMouseDrag()
+    // {
+    //     Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    //     transform.position = new Vector3(mousePos.x, mousePos.y, 0);
+    // }
 
     private void OnMouseUp()
     {
         Vector3 vectorForce = InitialPos - transform.position;
         GetComponent<Rigidbody2D>().AddForce(vectorForce * 300);
         GetComponent<Rigidbody2D>().gravityScale = 1;
+    }
+    private void OnMouseDown()
+    {
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = mousePosition;
     }
 }
