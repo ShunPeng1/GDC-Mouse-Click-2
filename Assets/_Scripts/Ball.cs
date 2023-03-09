8496dea4d67724400e6d2a245e15f67f48bc6d00
ShunPeng1 <shunpeng1@gmail.com>
2023-03-09 11:22:33 +0700

    feat: Ball only Push when on Platform

--- Assets/_Scripts/Ball.cs
+++ Assets/_Scripts/Ball.cs
@@ -5,33 +5,67 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    Vector3 _initialPos;
+    [Header("GameObject Properties")]
     private Rigidbody2D rb;
-    public float push;
 
+    
+    [Header("push Properties")]
+    [SerializeField] private float pushForce;
+    [SerializeField, Min(1)] private int maxPushEnergy = 1;
+    
+    private Vector3 _initialPos;
+    private bool _isPushing;
+    [SerializeField]private int _currentPushEnergy;
+    
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        _currentPushEnergy = maxPushEnergy;
     }
     
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            _initialPos = mousePosition;
-        }
 
         if (Input.GetMouseButtonUp(0))
         {
-            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Vector3 backwardDirection =  (_initialPos - mousePosition).normalized;
-            
-            rb.AddForce(backwardDirection * push);
-            rb.gravityScale = 1;
+            ExecutePush();
+        }
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            InitPush();   
         }
     }
-    
+
+    private void ExecutePush()
+    {
+        if (!_isPushing) return;
+        
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 backwardDirection =  (_initialPos - mousePosition).normalized;
+        
+        rb.AddForce(backwardDirection * pushForce);
+
+        _isPushing = false;
+
+    }
+
+    private void InitPush()
+    {
+        if (_currentPushEnergy <= 0) return;
+        _initialPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        _isPushing = true;
+        _currentPushEnergy--;
+    }
+
+    private void OnCollisionStay2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Platform"))
+        {
+            _currentPushEnergy = maxPushEnergy;
+        }
+    }
     
     
 }
