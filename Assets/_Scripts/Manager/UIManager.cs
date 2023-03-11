1e6b612c78e59bfafe3347888906887137a533ed
hathienhai4 <120299076+hathienhai4@users.noreply.github.com>
2023-03-11 17:26:22 +0700

    GameOver

--- Assets/_Scripts/Manager/UIManager.cs
+++ Assets/_Scripts/Manager/UIManager.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class UIManager : Singleton<UIManager>
 {
-    
+    static public UIManager instance;
+
+    public GameObject bnGameOver;
+
+    private void Awake()
+    {
+        UIManager.instance = this;
+        this.bnGameOver = GameObject.Find("bnGameOver");
+        this.bnGameOver.SetActive(false);
+        //UIManager.instance.bnGameOver.SetActive(true); (thêm vào khi thua)
+    }
     
 }
