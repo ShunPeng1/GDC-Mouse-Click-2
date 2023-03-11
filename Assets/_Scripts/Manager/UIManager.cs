4b4356ccf216c94026a95984b80508bddc6aa8bc
hathienhai4 <120299076+hathienhai4@users.noreply.github.com>
2023-03-11 20:56:26 +0700

    Restart4

--- Assets/_Scripts/Manager/UIManager.cs
+++ Assets/_Scripts/Manager/UIManager.cs
@@ -5,21 +5,9 @@ using TMPro;
 
 public class UIManager : Singleton<UIManager>
 {
-    
-    public GameObject bnGameOver;
-
     public TextMeshProUGUI text;
     int score;
-
-    private void Start()
-    {
-        this.bnGameOver = GameObject.Find("bnGameOver");
-        this.bnGameOver.SetActive(false);
-        //UIManager.instance.bnGameOver.SetActive(true); (thêm vào khi thua)
-    }
-    
-    
-
+  
     public void ChangeScore(int carrotValue)
     {
         text.text = "X" + carrotValue.ToString();
